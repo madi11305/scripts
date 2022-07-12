7dd14773252ed614f0638944937d7cd5876bfccf
madi11305 <madi11305@protonmail.com>
2022-07-12 16:07:14 -0700

    Debloated script

--- kanji.sh
+++ kanji.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 # Replaces day of the week with the appropriate Kanji in my slstatus bar
 
-[ $(date "+%a") == "Sun" ] && day="日"
-
-[ $(date "+%a") == "Mon" ] && day="月"
-
-[ $(date "+%a") == "Tue" ] && day="火"
-
-[ $(date "+%a") == "Wed" ] && day="水"
-
-[ $(date "+%a") == "Thu" ] && day="木"
-
-[ $(date "+%a") == "Fri" ] && day="金"
-
-[ $(date "+%a") == "Sat" ] && day="土"
+[ $(date "+%a") = "Sun" ] && day=日
+[ $(date "+%a") = "Mon" ] && day=月
+[ $(date "+%a") = "Tue" ] && day=火
+[ $(date "+%a") = "Wed" ] && day=水
+[ $(date "+%a") = "Thu" ] && day=木
+[ $(date "+%a") = "Fri" ] && day=金
+[ $(date "+%a") = "Sat" ] && day=土
 
 echo $day
